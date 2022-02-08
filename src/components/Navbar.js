79422cf6b3c2d02f2b146dd8654f6015e6d5f39e
Kristrún Jóhannsdóttir <krijo@itu.dk>
2022-02-08 20:45:21 +0100

    Some CSS and stuff. Not done though

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react"
-import { useNavigate, Link } from "react-router-dom"
-import { SidebarData } from "./SidebarData"
-import "./Navbar.css"
-import { IconContext } from "react-icons"
-import Parse from 'parse/dist/parse.min.js';
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { SidebarData } from "./SidebarData";
+import "./CSS/Navbar.css";
+import { IconContext } from "react-icons";
+import Parse from "parse/dist/parse.min.js";
 
 function Navbar() {
-//   const [sidebar, setSidebar] = useState(false)
+  //   const [sidebar, setSidebar] = useState(false)
 
-//   const showSidebar = () => setSidebar(!sidebar)
+  //   const showSidebar = () => setSidebar(!sidebar)
   const navigate = useNavigate();
 
   function handleSignOut(e) {
@@ -27,43 +27,41 @@ function Navbar() {
           </Link>
         </div> */}
         <nav className={"nav-menu"}>
-            {!Parse.User.current() && ( //when the user is not logged in.
-                <>
-                    <ul className="nav-menu-items">
-                        <li className="nav-text">
-                            <Link to="/">AsoPlan</Link>   
-                        </li>
-                        <li className="nav-text">
-                            <Link to="/signup">Sign Up</Link>   
-                        </li>
-                        <li className="nav-text">
-                            <Link to="/login">LogIn</Link>   
-                        </li>
-                    </ul>
-                </>
-            )}
+          {!Parse.User.current() && ( //when the user is not logged in.
+            <>
+              <ul className="nav-menu-items">
+                <li className="nav-title">
+                  <Link to="/">AsoPlan</Link>
+                </li>
+                <li className="nav-text">
+                  <Link to="/signup">Sign Up</Link>
+                </li>
+                <li className="nav-text">
+                  <Link to="/login">LogIn</Link>
+                </li>
+              </ul>
+            </>
+          )}
 
-            {Parse.User.current() && ( //when the user is logged in. 
-                <>
-                    <ul className="nav-menu-items">
-                        {SidebarData.map((item, index) => {
-
-                        return (
-                            <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                                <span>{item.title}</span>
-                            </Link>
-                            </li>
-                        )
-                        })}
-                    </ul>
-                </>
-            )}
-
+          {Parse.User.current() && ( //when the user is logged in.
+            <>
+              <ul className="nav-menu-items">
+                {SidebarData.map((item, index) => {
+                  return (
+                    <li key={index} className={item.cName}>
+                      <Link to={item.path}>
+                        <span>{item.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
         </nav>
       </IconContext.Provider>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
---
+++ src/components/Table.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./CSS/Table.css";
+
+function Table() {
+  return (
+    <>
+      <div className="table">
+        <div className="headerbox">
+          <div className="headertext">Hello</div>
+        </div>
+        <div className="headerbox">Hello</div>
+        <div className="headerbox">Hello</div>
+        <div className="textbox"></div>
+      </div>
+      <div className="table">
+        <div className="textbox">
+          {" "}
+          <div className="boxtext">Hello</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Table;

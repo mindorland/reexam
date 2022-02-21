d58666f7db34ae938262ca922d9356c17af7bc7f
Kristrún Jóhannsdóttir <krijo@itu.dk>
2022-02-21 12:08:29 +0000

    more css

--- src/pages/Cars/Cars.js
+++ src/pages/Cars/Cars.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { Link } from "react-router-dom";
-
 
 function Cars() {
   const navigate = useNavigate();
@@ -16,17 +15,30 @@ function Cars() {
     e.preventDefault();
     navigate("/cars/rider");
   }
-  
+
   return (
-    <div className='cars'>
-      <h1>Are you a driver or a rider for the excursion? </h1> 
+    <div className="cars">
+      <div class="card">
+        <div class="card-header">
+          Are you a driver or a rider for the excursion?
+        </div>
+        <div class="card-body">
+          <Button onClick={handleDriverClick} variant="primary" type="submit">
+            Driver
+          </Button>
+          <Button
+            className="primary-button"
+            onClick={handleRiderClick}
+            variant="primary"
+            type="submit"
+          >
+            Rider
+          </Button>
+        </div>
+        <div class="card-footer">Footer</div>
+      </div>
+      <h1> </h1>
       {/* To Do: change the excursion to the title? */}
-      <Button onClick={handleDriverClick} variant="primary" type="submit">
-        Driver
-      </Button>
-      <Button onClick={handleRiderClick} variant="primary" type="submit">
-        Rider
-      </Button>
     </div>
   );
 }
--- src/pages/Cars/Driver.js
+++ src/pages/Cars/Driver.js
@@ -1,40 +1,40 @@
-import React from "react"
-import { useState } from "react"
-import { Button, Form, Dropdown, DropdownButton } from "react-bootstrap"
-import { useNavigate } from "react-router"
-import { Link } from "react-router-dom"
-import Parse from "parse/dist/parse.min.js"
+import React from "react";
+import { useState } from "react";
+import { Button, Form, Dropdown, DropdownButton, Card } from "react-bootstrap";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import Parse from "parse/dist/parse.min.js";
 
 function Driver() {
-  const [seats, setSeats] = useState()
-  const [notes, setNotes] = useState()
-  const [isClosed, setIsClosed] = useState(false) // is it open case or not?
-  const navigate = useNavigate()
+  const [seats, setSeats] = useState();
+  const [notes, setNotes] = useState();
+  const [isClosed, setIsClosed] = useState(false); // is it open case or not?
+  const navigate = useNavigate();
 
-  const seatsValue = Number(seats) //to convert data values to Number
+  const seatsValue = Number(seats); //to convert data values to Number
 
   function handleSave(e) {
-    e.preventDefault()
+    e.preventDefault();
 
-    const Drives = new Parse.Object.extend("Drives") //create a new Parse Obejct subclass
-    const drive = new Drives() // Create a new instance
-    const currentUser = Parse.User.current() //get current user
+    const Drives = new Parse.Object.extend("Drives"); //create a new Parse Obejct subclass
+    const drive = new Drives(); // Create a new instance
+    const currentUser = Parse.User.current(); //get current user
 
-    drive.set("driver", currentUser) //to figure out!!
-    drive.set("fullSeats", seatsValue) //fullSeats is to store the total number of available seats
-    drive.set("remainingSeats", seatsValue) //to manage the remaining seats
-    drive.set("notes", notes)
-    drive.set("isClosed", isClosed)
+    drive.set("driver", currentUser); //to figure out!!
+    drive.set("fullSeats", seatsValue); //fullSeats is to store the total number of available seats
+    drive.set("remainingSeats", seatsValue); //to manage the remaining seats
+    drive.set("notes", notes);
+    drive.set("isClosed", isClosed);
 
     drive.save().then(
       (drive) => {
-        navigate("/cars/driverstatus")
-        alert("New object created")
+        navigate("/cars/driverstatus");
+        alert("New object created");
       },
       (error) => {
-        alert("Failed to create new object" + error.message)
+        alert("Failed to create new object" + error.message);
       }
-    )
+    );
   }
 
   //   function handleCancel(e) {
@@ -42,44 +42,51 @@ function Driver() {
   //   }
 
   function handleSelect(e) {
-    console.log(e)
-    setSeats(e)
+    console.log(e);
+    setSeats(e);
   }
 
   return (
-    <div className="cars">
-      <h1>Car Information </h1>
+    <>
 
-      <h3>Available Seats</h3>
-      <DropdownButton id="dropdown-basic-button" title="Dropdown button" onSelect={handleSelect}>
-        <Dropdown.Item eventKey="1">1</Dropdown.Item>
-        <Dropdown.Item eventKey="2">2</Dropdown.Item>
-        <Dropdown.Item eventKey="3">3</Dropdown.Item>
-        <Dropdown.Item eventKey="4">4</Dropdown.Item>
-        <Dropdown.Item eventKey="5">5</Dropdown.Item>
-      </DropdownButton>
-      <h4>You selected {seats} seats</h4>
-      {/* maybe change this to conditional rendering to show only if seats are not null */}
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicNotes">
-          <Form.Label>Notes</Form.Label>
-          <Form.Control
-            type="text"
-            onChange={(e) => setNotes(e.target.value)}
-            placeholder="i.e., Departure place/time"
-            autoFocus
-          />
-        </Form.Group>
-      </Form>
-      <Button onClick={handleSave} variant="primary" type="submit">
-        Save
-      </Button>
-      {/* <Button onClick={handleCancel} variant="primary" type="submit">
+      <div className="cars">
+        <h1>Car Information </h1>
+
+        <h3>Available Seats</h3>
+        <DropdownButton
+          id="dropdown-basic-button"
+          title="Dropdown button"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item eventKey="1">1</Dropdown.Item>
+          <Dropdown.Item eventKey="2">2</Dropdown.Item>
+          <Dropdown.Item eventKey="3">3</Dropdown.Item>
+          <Dropdown.Item eventKey="4">4</Dropdown.Item>
+          <Dropdown.Item eventKey="5">5</Dropdown.Item>
+        </DropdownButton>
+        <h4>You selected {seats} seats</h4>
+        {/* maybe change this to conditional rendering to show only if seats are not null */}
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicNotes">
+            <Form.Label>Notes</Form.Label>
+            <Form.Control
+              type="text"
+              onChange={(e) => setNotes(e.target.value)}
+              placeholder="i.e., Departure place/time"
+              autoFocus
+            />
+          </Form.Group>
+        </Form>
+        <Button onClick={handleSave} variant="primary" type="submit">
+          Save
+        </Button>
+        {/* <Button onClick={handleCancel} variant="primary" type="submit">
             Cancel
         </Button> */}
-      {/* To Do: clear the form. Maybe have to use 3rd party library....  */}
-    </div>
-  )
+        {/* To Do: clear the form. Maybe have to use 3rd party library....  */}
+      </div>
+    </>
+  );
 }
 
-export default Driver
+export default Driver;
